dd9f312a8b4564d38a9d7681f138004fb5abe7c4
Clark Walker <303walker@gmail.com>
2021-08-10 18:15:09 -0600

    add calling cpp functions to test them out

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 
-WebAssembly.instantiateStreaming(fetch("/bin/lib_target.wasm"), {})
+WebAssembly.instantiateStreaming(
+    fetch("/bin/lib_target.wasm"), {})
 .then( response => {
     console.log( response );
-    // console.log( "results of wasm functions below" );
-    // console.log( "30 + 12 : ", response.instance.exports.my_add(30, 12) );
-    // console.log( "30 - 12 : ", response.instance.exports.my_subtract(30, 12) );
-    // console.log( "30th fib: ", response.instance.exports.nth_fibonacci(30) );
+    console.log( "results of wasm functions below" );
+    console.log( "30 + 12 : ", response.instance.exports.my_add(30, 12) );
+    console.log( "30 - 12 : ", response.instance.exports.my_subtract(30, 12) );
+    console.log( "30th fib: ", response.instance.exports.nth_fibonacci(30) );
 });
