dfa9f03e304c7a4393a692f14297aaa5bc9eb8c9
Clark Walker <303walker@gmail.com>
2021-08-10 18:44:45 -0600

    make prettier code

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 
+function js_nth_fibonacci(n) {
+    if (n <= 1) {
+        return 1;
+    }
+    return js_nth_fibonacci( n - 1 ) + js_nth_fibonacci( n - 2 );
+}
+
 WebAssembly.instantiateStreaming(
     fetch("/bin/lib_target.wasm"), {})
 .then( response => {
@@ -6,13 +13,18 @@ WebAssembly.instantiateStreaming(
     console.log( "results of wasm functions below" );
     console.log( "30 + 12 : ", response.instance.exports.my_add(30, 12) );
     console.log( "30 - 12 : ", response.instance.exports.my_subtract(30, 12) );
-    console.log( "30th fib: ", response.instance.exports.nth_fibonacci(30) );
 
     console.time("cpp_nth_fibonacci");
-    console.log( response.instance.exports.nth_fibonacci(30) );
+    console.log(
+        "30th fibonacci number: ",
+        response.instance.exports.nth_fibonacci(30)
+    );
     console.timeEnd("cpp_nth_fibonacci");
 
     console.time("js_nth_fibonacci");
-    console.log( js_nth_fibonacci(30) );
+    console.log(
+        "30th fibonacci number: ",
+        js_nth_fibonacci(30)
+    );
     console.timeEnd("js_nth_fibonacci");
 });
