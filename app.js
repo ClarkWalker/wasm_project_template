86f6662b23e7780a664b51cbdfa0010260c92074
Clark Walker <303walker@gmail.com>
2021-08-21 20:37:25 -0600

    completely changed the role of this file, it's now acting like server.js was and server.js is now depricated

--- app.js
+++ app.js
@@ -1,89 +1,17 @@
-// /* ////////////////////////////////////////////////////////////
-// table
-const table = new WebAssembly.Table({
-    initial: 2,
-    element: "anyfunc"
-});
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8000;
 
-WebAssembly.instantiateStreaming(
-    fetch("/bin/lib_target.wasm"))
-.then((result) => {
-    console.log(result);
-    table.set(0, result.instance.exports.nth_fibonacci);
-    table.set(1, result.instance.exports.my_add);
-    table.grow(2, result.instance.exports.my_subtract);
-    table.grow(3, result.instance.exports.reverse_array);
-    console.log(table.get(0)(30));
-    console.log(table.get(1)(9, 5));
-    console.log(table.get(2)(9, 5));
-    console.log(table.get(3)([5, 4, 3, 2, 1]));
-});
+app.use(express.static(__dirname + ""));
 
+express.static.mime.types[".wasm"] = "application/wasm";
 
-/////////////////////////////////////////////////////////////*/
-///////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////
-/* ////////////////////////////////////////////////////////////
-// memory
-let memory = new WebAssembly.Memory({
-    initial: 128,
-    maximum: 128
-});
-
-// memory.grow(10); // grows memory by 10 64 KB pages, thorws error if over max
-let arr = new Uint32Array(memory.buffer);
-
-const importObject = {
-    env: {
-        memory: memory
+const server = app.listen(port, (err) => {
+    if (err) {
+        res.send("closing..");
+        app.close();
     }
-};
-
-WebAssembly.instantiateStreaming(
-    fetch("/bin/lib_target.wasm"), importObject)
-.then(({instance, module}) => {
-    for(let i = 0; i < 30; i++) {
-        arr[i] = i + 1;
-    }
-    instance.exports.reverse_array(0, 30);
-    // console.log(arr);
+    console.log(`Server running on port: ${port}`);
 });
 
-
-/////////////////////////////////////////////////////////////*/
-///////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////
-/* ////////////////////////////////////////////////////////////
-// primary
-
-const js_nth_fibonacci = function (n) {
-    if (n <= 1) {
-        return 1;
-    }
-    return js_nth_fibonacci( n - 1 ) + js_nth_fibonacci( n - 2 );
-}
-
-WebAssembly.instantiateStreaming(
-    fetch("/bin/lib_target.wasm"), {})
-.then( response => {
-    console.log( response );
-    console.log( "results of wasm functions below" );
-    console.log( "30 + 12 : ", response.instance.exports.my_add(30, 12) );
-    console.log( "30 - 12 : ", response.instance.exports.my_subtract(30, 12) );
-
-    console.time("cpp_nth_fibonacci");
-    console.log(
-        "30th fibonacci number: ",
-        response.instance.exports.nth_fibonacci(30)
-    );
-    console.timeEnd("cpp_nth_fibonacci");
-
-    console.time("js_nth_fibonacci");
-    console.log(
-        "30th fibonacci number: ",
-        js_nth_fibonacci(30)
-    );
-    console.timeEnd("js_nth_fibonacci");
-});
-
-///////////////////////////////////////////// */
+module.exports = server;
